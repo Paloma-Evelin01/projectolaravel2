4b64ff8a1282f8bd924afb671c52668110b9e41c
Paloma-Evelin01 <79293332+Paloma-Evelin01@users.noreply.github.com>
2021-03-02 01:03:54 -0600

    Add files via upload

---
+++ ListaController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\estudiantes; 
+
+class ListaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+         
+        $estudiante=estudiantes::all();
+        //return $estudiante;
+        return view ('VistaEstudiantes', compact('estudiante'));
+        
+        //$estudiante= estudiantes::all();
+        //return view ('VistaEstudiantes', compact('estudiante'));
+        //esto es de la practica 2
+        $texto=trim($request->get ("texto"));
+        $estudiante=DB::table ('estudiantes')->select('Matricula','Nombre','Direccion')->where ('matricula','=',$texto)->paginate(10);
+        return view ('VistaEstudiantes', compact('estudiante'));
+        
+        /*$texto=trim($request->get ("texto"));
+        $estudiante=DB::table ('estudiantes')->select('Matricula','Nombre','Direccion')->where ('nombre','LIKE',"%$texto%")->paginate(10);
+        return view ('VistaEstudiantes', compact('estudiante'));
+        */
+    }
+    
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    $estudiante= estudiantes::find($id);
+    //dd($estudiante);
+    $estudiante->delete();
+    return redirect()->route('Lista.index');
+    
+    }
+}
---
+++ web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//esto ya estaba
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+/*
+Route::get('datos',function(){
+    return view('datos');
+});
+*/
+Route::get('/datos','datosController@mostrar');
+Route::post('/datos','datosController@store')->name('datos.store');
+
+Route::get('/contacto','ContactController@mostrar');
+Route::post('/contacto','ContactController@store')->name('contacto.store');
+
+Route::get('/estudiantes','ControllerEstudiantes@mostrar');
+Route::post('/estudiantes','ControllerEstudiantes@store')->name('estudiantes.store');
+
+Route::resource('/Lista',ListaController::class);
+Route::delete('Lista/{matricula}', 'ListaController@destroy')
+->name('Lista.destroy');
+
